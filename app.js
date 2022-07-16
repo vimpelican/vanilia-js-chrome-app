6b4d265e017d69531d6bcd3a5316d9a8488674c5
vimpelican <snustcice@seoultech.ac.kr>
2022-07-16 19:59:43 +0900

    #2.4 Booleans

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-console.log("Hello World!"); // prints Hello World in console
-
-const a = 5;    // const variable
-const b = 2;
-const veryLongVariableName = 0; // CamelCase - normal naming practice in JS
-let myName = "Gyejin"
-// Always use const, sometimes use let, never use var
-
-console.log(a+b);   // better calculator
-console.log(a*b);
-console.log(a/b);
-console.log("Hello "+myName);
-
-myName = "GJ";  // update myName
-
-console.log("Your new name is " + myName);
+const amIFat = null;    // null never happens automatically - make sure "nothing here!""
+let something;  // exists, no value here
+console.log(something, amIFat);
