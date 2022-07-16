edaa6ac2153beaa1510332270f72d71dc72c78eb
vimpelican <snustcice@seoultech.ac.kr>
2022-07-16 19:39:12 +0900

    #2.2 Variables

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-alert("hi");
+console.log("Hello World!"); // prints Hello World in console
+
+const a = 5;    // const variable
+const b = 2;
+const veryLongVariableName = 0; // CamelCase - normal naming practice in JS
+const myName = "Gyejin"
+
+console.log(a+b);   // better calculator
+console.log(a*b);
+console.log(a/b);
+console.log("Hello "+myName);
