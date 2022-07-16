49572545105f5258e9ed1f7ddaaa8a9568640d3f
vimpelican <snustcice@seoultech.ac.kr>
2022-07-16 19:50:13 +0900

    #2.3 const and let

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ console.log("Hello World!"); // prints Hello World in console
 const a = 5;    // const variable
 const b = 2;
 const veryLongVariableName = 0; // CamelCase - normal naming practice in JS
-const myName = "Gyejin"
+let myName = "Gyejin"
+// Always use const, sometimes use let, never use var
 
 console.log(a+b);   // better calculator
 console.log(a*b);
 console.log(a/b);
 console.log("Hello "+myName);
+
+myName = "GJ";  // update myName
+
+console.log("Your new name is " + myName);
