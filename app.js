48c9fd99289716ab05726da3b0f53372bff54f1d
vimpelican <snustcice@seoultech.ac.kr>
2022-07-17 17:18:32 +0900

    #2.5 Arrays

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const mon = "mon";
-const tue = "tue";
-const wed = "wed";
-const thu = "thu";
-const fri = "fri";
-const sat = "sat";
-const sun = "sun";
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 
-const daysOfWeek = [mon, tue, wed, thu, fri, sat, sun];
+// Get Items from Array
+console.log(daysOfWeek);
 
-const nonSense = [1, 2, "hello", false, null, true, undefined, "gyejin"];
+// Add one more day to the array
+daysOfWeek.push("sun");
 
-console.log(daysOfWeek, nonSense);
+console.log(daysOfWeek);
+
+const toBuy = ["tomato", "potato", "pizza"];
+
+toBuy.push("kimbab");
+
+console.log(toBuy[9999999]);    //undefined
