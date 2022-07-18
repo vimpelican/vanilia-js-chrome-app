a695fdff700fe581e786060c78276fe595b3389c
vimpelican <snustcice@seoultech.ac.kr>
2022-07-18 12:15:08 +0900

    Return value

--- app.js
+++ app.js
@@ -1,23 +1,7 @@
-// Challenge
-const calculator = {
-    add: function(firstNum, secondNum){
-        console.log(firstNum+secondNum);
-    },
-    subtract: function(firstNum, secondNum){
-        console.log(firstNum-secondNum);
-    },
-    multiple: function(firstNum, secondNum){
-        console.log(firstNum*secondNum);
-    },
-    divide: function(firstNum, secondNum){
-        console.log(firstNum*secondNum);
-    },
-    powerof: function(firstNum, secondNum){
-        console.log(firstNum**secondNum);
-    },
+const age = 96;
+function calculateKrAge(ageOfForeigner){
+    return ageOfForeigner + 2;
 }
 
-calculator.add(2,3);
-calculator.subtract(2,3);
-calculator.multiple(2,3);
-calculator.powerof(2,3);
+const krAge = calculateKrAge(age);
+console.log(krAge);
