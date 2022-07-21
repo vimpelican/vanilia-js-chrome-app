029d5ae98dec83ef31a4947a9db5f1e6f60b92a8
vimpelican <snustcice@seoultech.ac.kr>
2022-07-21 12:07:28 +0900

    #3.0 The Document Object

--- app.js
+++ app.js
@@ -1,15 +1,2 @@
-const age = parseInt(prompt("How old are you?"));
-
-if (isNaN(age) || age < 0) {
-    console.log("Please write a real positive number!");
-} else if (age < 18) {
-    console.log("You are too young.");
-} else if (age >= 18 && age <= 50) { // AND operator
-    console.log("You can drink.");
-} else if (age > 50 && age <= 80) {
-    console.log("You should exercise");
-} else if (age === 100) {
-    console.log("Wow you're wise!");
-} else if (age > 80) {
-    console.log("You can do whatever you want.");
-}
+// JS is already connected to HTML
+document.title = "Hello from JS!"
